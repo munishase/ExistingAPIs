d114db4e648913ad38260f0a1e4a0a8a4751225f
Munish <47652113+munishase@users.noreply.github.com>
2020-05-04 20:13:17 +1000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 const server = new app().Start(port)
   .then(port => console.log(`Server running on port ${port}`))
