f945a6132d5ddc25b5608a827e74578e032d9718
Munish <munish.singla@aseit.com.au>
2020-05-08 10:51:47 +1000

    save logs/records to database mongodb

--- src/Services/DbCrudOperations.ts
+++ src/Services/DbCrudOperations.ts
@@ -1,6 +1,3 @@
-import { EnumPartOf } from '../Enum/EnumPartOf'
-import { Logger } from '../class/Logger'
-import { EnumModule } from '../Enum/EnumModule';
 import { BaseLayer } from './BaseLayer';
 var MongoClient = require('mongodb').MongoClient;
 
@@ -8,24 +5,30 @@ var MongoClient = require('mongodb').MongoClient;
 //this class will contain all the functions, used to interation with db
 class DbCrudOperations extends BaseLayer {
 
+
   //generate random password with length as input param
   saveRecord(record: any) {
-    var dbConnectionString = this.environmentConfig.database.Url;
-    console.log(dbConnectionString)
-    MongoClient.connect(dbConnectionString, function (error: any, dbConnection: any) {
-      if (error)
-        throw error;
-
-      var myStudent = { name: "test data" };
-      dbConnection.collection("fluid").insertOne(myStudent, function (error: any, result: any) {
+    console.log(this.environmentConfig.database.enabled)
+    if (this.environmentConfig.database.enabled == true) {
+      MongoClient.connect(this.dbConnectionString(), function (error: any, dbConnection: any) {
         if (error)
           throw error;
-        
-        console.log("1 Recorded Inserted");
-        dbConnection.close();
-      });
-    });
 
+        dbConnection.collection("fluid").insertOne(record, function (error: any, result: any) {
+          if (error)
+            throw error;
+
+          dbConnection.close();
+        });
+      });
+    }
+  }
+
+  dbConnectionString() {
+    let userpwd = "";
+    if (this.environmentConfig.database.username != "")
+      userpwd = this.environmentConfig.database.username + ":" + this.environmentConfig.database.password + "@";
+    return this.environmentConfig.database.urlprefix + userpwd + this.environmentConfig.database.url;
   }
 
 }
