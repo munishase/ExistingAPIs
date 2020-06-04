5f19297fee2c9843e4b245c249caba347e0fbdc8
Daniel ward <dan@deltaxent.com.au>
2020-06-04 14:07:34 +1000

    Chore: Add node to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,20 @@ module.exports = {
     ],
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+        "plugin:node/recommended",
+        'plugin:@typescript-eslint/recommended'
     ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        "node/prefer-global/buffer": ["error", "always"],
+        "node/prefer-global/console": ["error", "always"],
+        "node/prefer-global/process": ["error", "always"],
+        "node/prefer-global/url-search-params": ["error", "always"],
+        "node/prefer-global/url": ["error", "always"],
+        "node/prefer-promises/dns": "error",
+        "node/prefer-promises/fs": "error",
+        "node/no-unsupported-features/es-syntax": "off"
+    }
 };
